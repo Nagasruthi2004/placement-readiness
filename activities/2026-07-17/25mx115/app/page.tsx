6263a38aaef37e46ca08c94c029e7e15687561dc
krishnapriya094 <25mx115@psgtech.ac.in>
2026-07-18 11:52:29 +0530

    bdg

--- activities/2026-07-17/25mx115/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import DynamicForm from "@/components/DynamicForm";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-100 p-10">
-      <DynamicForm />
-    </main>
-  );
-};
